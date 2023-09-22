2367a8a33f986b89950f8e29048021f4c09039ae
KELVIN MURITHI <kelvinmurithi@live.com>
2023-09-22 15:55:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+  
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+  
+  
